4905c9034d2ed6d7c154afbe3df4ff113368eb30
CASY82 <repaid11010@gmail.com>
2023-03-16 01:54:49 +0900

    add : 20230316 - section6 25pg

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
   <ol>
     <li v-for="item in list" :key="item.id">{{ item }}</li>
   </ol>
+  <input v-model="message">
+  <input v-model.number="count">
+  <p v-if="show">Hello world!</p>
+
+  <button v-on:click="show=!show">변경하기</button>
+  <transition>
+    <p v-if="show">나는 안보였다가 보였다가!</p>
+  </transition>
+
 </template>
 
 <script>
@@ -10,7 +19,9 @@ export default {
   data() {
         return {
             message: 'Hello Vue3!',
-            list: ['사과', '바나나', '딸기']
+            list: ['사과', '바나나', '딸기'],
+            count: 1,
+            show: true
         }
     },
   // 데이터 추가하는 방법
@@ -25,5 +36,11 @@ export default {
 <style>
 #app{
   
+}
+.v-enter-active, .v-leave-active{
+  transition: opacity 1s;
+}
+.v-enter, .v-leave-to{
+  opacity: 0;
 }
 </style>
